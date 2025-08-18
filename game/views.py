39e8fc1d9a9362e5cc452ddc9d64a47abc9bbae8
minami <south614fruit@yahoo.co.jp>
2025-08-18 22:42:26 +0900

    やることルーレットに変更&DBで表示項目を管理

--- game/views.py
+++ game/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import RouletteItem
 
 def roulette(request):
-    return render(request, "game/roulette.html")
+    items = list(RouletteItem.objects.values_list("label", flat=True))
+    return render(request, "game/roulette.html", {"items": items})
